8623a819dcab8d559820a312aa397afbdaf257a1
Akshay Patel <akshayrpatel@gmail.com>
2015-04-10 14:44:54 -0700

    Pre compute component for custom components

--- benchmark/createElement.js
+++ benchmark/createElement.js
@@ -5,46 +5,42 @@ var React12Min = require('../react-0.12/dist/react.min');
 var React13 = require('../react-0.13/react');
 var React13Min = require('../react-0.13/dist/react.min');
 var Benchtable = require('benchtable');
-var suite = new Benchtable();
+var suite = new Benchtable("react", { "isTransposed": true });
 
-suite
-.addFunction('createElement', function(react) {
-    var element = react.createElement('h1', null, 'Hello, world!');
-    react.renderToString(element);
-})
-.addFunction('createClass', function(react) {
-    var HelloWorld = react.createClass({
-        displayName: 'CommentBox',
+function makeComponent(react) {
+    var component = react.createClass({
+        displayName: 'HelloWorld',
         render: function() {
             return (react.createElement('div', { className: "hello" }, "Hello, world!"));
         }
     });
 
-    var element = react.createElement(HelloWorld, null);
+    return component;
+}
+
+var helloWorldComponents = {
+    '12': makeComponent(React12),
+    '12min': makeComponent(React12Min),
+    '13': makeComponent(React13),
+    '13min': makeComponent(React13Min),
+};
+
+suite
+.addFunction('DOM', function(react) {
+    var element = react.createElement('h1', null, 'Hello, world!');
     react.renderToString(element);
 })
-.addInput('React 12', [React12])
-.addInput('React 12 Min', [React12Min])
-.addInput('React 13', [React13])
-.addInput('React 13 Min', [React13Min])
+.addFunction('Component', function(react, version) {
+    var component = helloWorldComponents[version];
+    var element = react.createElement(component, null);
+    react.renderToString(element);
+})
+.addInput('React 12', [React12, '12'])
+.addInput('React 12 Min', [React12Min, '12min'])
+.addInput('React 13', [React13, '13'])
+.addInput('React 13 Min', [React13Min, '13min'])
 .on('complete', function() {
     console.log('The Fastest test suite is ' + '\u001b[32m' + this.filter('fastest').pluck('name') + '\u001b[0m\n');
     console.log(this.table.toString());
-
-    /*function compare(a, b) {
-        if (a > b) {
-            return ( a / b * 100).toFixed() + '% faster';
-        }
-
-        if (a === b) {
-            return 'the same';
-        }
-
-        return (b / a * 100).toFixed() + '% slower';
-    }
-
-    console.log('React12Min is ' + compare(this[1].hz, this[0].hz) + ' vs React12\n');
-    console.log('React13Min is ' + compare(this[3].hz, this[2].hz) + ' vs React12');
-    console.log('React13Min is ' + compare(this[3].hz, this[1].hz) + ' vs React12Min');*/
 })
 .run({ 'async': true });
