820cc786a3d0608159b4e0716c18cde3147cb436
Akshay Patel <akshayrpatel@gmail.com>
2015-11-05 12:38:01 -0800

    React 14 benchmarks

--- benchmark/createElement.js
+++ benchmark/createElement.js
@@ -4,6 +4,8 @@ var React12 = require('../react-0.12/react');
 var React12Min = require('../react-0.12/dist/react.min');
 var React13 = require('../react-0.13/react');
 var React13Min = require('../react-0.13/dist/react.min');
+var React14 = require('../react-0.14/dist/react');
+var React14Min = require('../react-0.14/dist/react.min');
 var Benchtable = require('benchtable');
 var suite = new Benchtable("react", { "isTransposed": true });
 
@@ -23,6 +25,8 @@ var helloWorldComponents = {
     '12min': makeComponent(React12Min),
     '13': makeComponent(React13),
     '13min': makeComponent(React13Min),
+    '14': makeComponent(React14),
+    '14min': makeComponent(React14Min)
 };
 
 suite
@@ -39,6 +43,8 @@ suite
 .addInput('React 12 Min', [React12Min, '12min'])
 .addInput('React 13', [React13, '13'])
 .addInput('React 13 Min', [React13Min, '13min'])
+.addInput('React 14', [React14, '14'])
+.addInput('React 14 Min', [React14Min, '14'])
 .on('complete', function() {
     console.log('The Fastest test suite is ' + '\u001b[32m' + this.filter('fastest').pluck('name') + '\u001b[0m\n');
     console.log(this.table.toString());
